c5bb4df4cc11c39abdc574101db5d1e84c48c697
Tristram Newman <tristram.newman@gmail.com>
2023-08-03 23:36:15 +0100

    Find missing words in deck

--- lib.py
+++ lib.py
@@ -1,3 +1,45 @@
 """
 Logic concerning Anki interactions via the AnkiConnect HTTP server.
 """
+
+
+# std lib
+import http
+import json
+from typing import Dict, Any
+
+# third party
+import requests
+
+
+ENDPOINT_URL = "http://localhost:8765/"
+
+
+class AnkiError(Exception):
+    pass
+
+class BadResponse(AnkiError):
+    pass
+
+class InternalError(AnkiError):
+    pass
+
+
+def anki(action, **params) -> Any:
+    req = {'action': action, 'params': params, 'version': 6}
+    req_json = json.dumps(req).encode('utf-8')
+    res = requests.post(ENDPOINT_URL, data=req_json)
+    if res.status_code != http.HTTPStatus.OK:
+        raise BadResponse(f"Server returned {res.status_code} {res.reason}")
+    res_json = res.json()
+
+    if len(res_json) != 2:
+        raise BadResponse(f"Invalid number of fields in reponse: {res_json=}")
+    if "error" not in res_json:
+        raise BadResponse(f"No 'error' field in response: {res_json=}")
+    if "result" not in res_json:
+        raise BadResponse(f"No 'result' field in response: {res_json=}")
+    if res_json["error"] is not None:
+        raise InternalError(f"Unexpected error: {res_json=}")
+
+    return res_json["result"]
--- search_deck.py
+++ search_deck.py
@@ -1,5 +1,5 @@
 """
-Searches for a list of vocabulary words in a named Anki deck.
+Searches for a list of vocabulary words in a named Anki deck and reports missing ones.
 
 Usage:
     search_deck.py
@@ -9,9 +9,11 @@ Usage:
 
 
 # std lib
+import os
 import csv
+import json
 import argparse
-from typing import List, Tuple
+from typing import List, Set, Dict, Tuple, Any
 
 # third party
 import requests
@@ -20,15 +22,20 @@ import requests
 import lib
 
 
-def load_words(file_path: str) -> List[Tuple[str, str]]:
-    words = []
+def is_katakana(text: str) -> bool:
+    # TODO
+    return False
+
+
+def load_words(file_path: str) -> Set[Tuple[str, str]]:
+    words = set()
 
     with open(file_path, "r", encoding="utf-8") as f:
         for row in csv.reader(f):
             if len(row) != 2:
                 print(f"WARNING: Unexpected row length {len(row)} for {row=}")
                 continue
-            words.append((row[0], row[1]))
+            words.add((row[0], row[1]))
 
     return words
 
@@ -39,7 +46,53 @@ if __name__ == "__main__":
     parser.add_argument("--deck", type=str, default="Japanese Core 10K +Pics +Aud +Pitch")
     parser.add_argument("--kanji-field", type=str, default="vocab")
     parser.add_argument("--kana-field", type=str, default="vocab-kana")
+    parser.add_argument("--output", type=str, default="missing_words.csv")
+    parser.add_argument("--verbose", "-v", action="store_true")
     args = parser.parse_args()
 
-    # Each word is (kanji, kana) tuple
-    words: List[Tuple[str, str]] = load_words(args.word_list_csv)
+    assert not os.path.exists(args.output), f"Output file '{args.output}' already exists"
+
+    # Check the deck exists
+    decks = lib.anki("deckNames")
+    assert args.deck in decks, f"Deck '{args.deck}' not found in Anki ({decks=})"
+
+    # Get all the notes
+    note_ids: List[int] = lib.anki("findNotes", query=f'"deck:{args.deck}"')
+    notes: List[Dict[str, Any]] = lib.anki("notesInfo", notes=note_ids)
+
+    # Fetch all the kanji/kana from the notes
+    words = set()
+    for note in notes:
+        note_id = note["noteId"]
+        fields = note["fields"]
+        if args.kanji_field not in fields or args.kana_field not in fields:
+            if args.verbose:
+                pretty_note = json.dumps(note, indent=4)
+                print(f"Note {note_id} is missing kana and/or kanji fields:\n{pretty_note}\n")
+            else:
+                print(f"Note {note_id} is missing kana and/or kanji fields")
+            continue
+
+        kanji = fields[args.kanji_field]["value"]
+        kana = fields[args.kana_field]["value"]
+
+        if is_katakana(kanji):
+            kana = kanji
+
+        if kanji == kana:
+            kanji = ""
+
+        words.add((kanji, kana))
+
+    # Find which ones are missing
+    missing_words = load_words(args.word_list_csv).difference(words)
+    print(f"{len(missing_words)} words are missing from deck '{args.deck}'")
+
+    with open(args.output, "w", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        for word in missing_words:
+            writer.writerow([word[0], word[1]])
+    print(f"Words written to {args.output}")
+
+    if args.verbose:
+        print(f"{missing_words=}")
